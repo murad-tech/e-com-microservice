9d890fa63ae6cf34f9cdf0978e99063c10220ef8
muratj <muratj.tm@gmail.com>
2025-03-22 07:10:59 -0500

    Request validation for order service

--- services/order-service/src/main/java/com/murad/order_service/dto/ProductItemsRequest.java
+++ services/order-service/src/main/java/com/murad/order_service/dto/ProductItemsRequest.java
@@ -2,9 +2,13 @@ package com.murad.order_service.dto;
 
 import com.murad.order_service.models.Order;
 import com.murad.order_service.models.ProductItems;
+import jakarta.validation.constraints.NotNull;
 
 public record ProductItemsRequest(
+    @NotNull(message = "Product id is required")
     Long productId,
+
+    @NotNull(message = "Quantity is required")
     Integer quantity
 ) {
     public ProductItems toProductItems(Order order) {
